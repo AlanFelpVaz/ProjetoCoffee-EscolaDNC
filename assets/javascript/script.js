92785d1f7238638b0d1f010251f02cbe5c7d7665
Alan Felipe <alanf.e1601@gmail.com>
2023-01-25 22:56:22 -0300

    adicionando botoes ao mouser entrar em cards

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+function mouseEnter(){
+
+const cards = document.querySelectorAll('.cards-over');
+
+const buttons = document.querySelectorAll('.cardBtn')
+
+cards.forEach(button =>{
+
+    button.addEventListener('mouseover', (event) =>{
+        event.currentTarget.closest('.cards-over').querySelector('.cardBtn').style.display = "block";
+    });
+    
+    button.addEventListener('mouseout', (event) =>{
+        event.currentTarget.closest('.cards-over').querySelector('.cardBtn').style.display = "none";
+    });
+
+});
+
+}
+
+mouseEnter();
+
